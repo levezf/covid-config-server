ae92d19b83d2bac3e034d688e39bc1f49a476570
Felipe Levez <levezf@gmail.com>
2020-12-20 21:22:32 -0300

    Ajuste nas propriedades do projeto

--- src/main/java/br/com/levez/configserver/ConfigServerApplication.java
+++ src/main/java/br/com/levez/configserver/ConfigServerApplication.java
@@ -1,8 +1,10 @@
 package br.com.levez.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableConfigServer
 @SpringBootApplication
